17597f5700adb834abf5e120cab4ac75051783b5
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-03-17 19:39:30 +0300

    fully added functionality

--- script.js
+++ script.js
@@ -14,12 +14,58 @@ const rangeClass = document.querySelector('.rangeClass');
 
 
 
+let toggle = document.querySelector('.toggle');
+
+function toogleFun(){
+    toggle.classList.toggle("active");
+
+}
+
+
+function mainFun(){
+    if(toggle.classList.contains("active")){
+        discountedRange();
+    }else{
+        basicRange();
+    }
+}
 
 
 
-function myFunction() {
-    let x = document.querySelector(".rangeClass");
-    let currentVal = parseFloat(x.value);
+
+function discountedRange(){
+    let baseRange = rangeClass;
+    let currentDiscountValue = parseFloat(baseRange.value);
+
+    if(currentDiscountValue == 8){
+            pageViewers.innerHTML = "10K";
+            outputPrice.innerHTML = "72";
+    
+    }else if(currentDiscountValue <= 12){
+            pageViewers.innerHTML = "50K";
+            outputPrice.innerHTML = "108";
+    
+        }else if(currentDiscountValue <= 16){
+            pageViewers.innerHTML = "100K";
+            outputPrice.innerHTML = "144";
+    
+        }else if(currentDiscountValue <= 24){
+
+            pageViewers.innerHTML = "500K";
+            outputPrice.innerHTML = "216";
+    
+        }else{
+            pageViewers.innerHTML = "1M";
+            outputPrice.innerHTML = "324";
+        }
+} 
+
+
+
+
+function basicRange() {
+    let inputRange = document.querySelector(".rangeClass");
+    let currentVal = parseFloat(inputRange.value);
 
     if(currentVal == 8){
         pageViewers.innerHTML = "10K";
@@ -29,7 +75,6 @@ function myFunction() {
         pageViewers.innerHTML = "50K";
         outputPrice.innerHTML = "12";
 
-
     }else if(currentVal <= 16){
         pageViewers.innerHTML = "100K";
         outputPrice.innerHTML = "16";
@@ -38,15 +83,8 @@ function myFunction() {
         pageViewers.innerHTML = "500K";
         outputPrice.innerHTML = "24";
 
-
     }else{
         pageViewers.innerHTML = "1M";
         outputPrice.innerHTML = "36";
-
     }
-
-
-
-    console.log(typeof currentVal)
-
   }
